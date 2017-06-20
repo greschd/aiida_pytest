84cc6beec4efa51de78c4c95d5c01c656f7c6433
Dominik Gresch <greschd@gmx.ch>
2017-06-20 09:37:20 +0200

    Use verdilib.Setup to run DB migrations

--- aiida_pytest/db_fixture.py
+++ aiida_pytest/db_fixture.py
@@ -1,22 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import division, print_function, unicode_literals
+
+import io
 import os
+import sys
+import contextlib
 from temporary import temp_dir
 
 import aiida
 import pytest
 from pgtest.pgtest import PGTest
 
+@contextlib.contextmanager
+def redirect_stdout(target):
+    original = sys.stdout
+    sys.stdout = target
+    yield
+    sys.stdout = original
+
+@contextlib.contextmanager
+def redirect_stdin(target):
+    original = sys.stdin
+    sys.stdin = target
+    yield
+    sys.stdin = original
+
 @pytest.fixture(scope='session')
 def aiidadb():
     with PGTest() as pgt, temp_dir() as td:
         monkeypatch_config(pg_port=pgt.port, repo_path=str(td))
         aiida.load_dbenv()
-        # from aiida.cmdline.verdilib import setup
-        # Setup().run()
-        # from aiida.cmdline.commands.code import Code
-        # Code().code_setup()
+        # avoid double load_dbenv
+        aiida.load_dbenv = lambda: None
+        run_setup()
         yield
 
 def monkeypatch_config(pg_port, repo_path):
@@ -31,3 +49,9 @@ def monkeypatch_config(pg_port, repo_path):
         }
     aiida.common.setup.get_config = get_test_config
     aiida.common.AIIDA_CONFIG_FOLDER = os.path.abspath(repo_path)
+
+def run_setup():
+    from aiida.cmdline.verdilib import Setup
+    # Python 3: contextlib.redirect_stdout
+    with open(os.devnull, 'w') as devnull, redirect_stdout(devnull), redirect_stdin(io.StringIO('N\n')):
+        Setup().run()
