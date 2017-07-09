63ad1dceeccf01640b3c3597f0dcd0af3a30f6f6
Vladimir <dikan.va@ya.ru>
2017-07-09 17:38:12 +0200

    remove submit_test folder

--- aiida_pytest/configure.py
+++ aiida_pytest/configure.py
@@ -20,10 +20,12 @@ from .contextmanagers import redirect_stdin, redirect_stdout
 
 __all__ = ['configure', 'configure_with_daemon']
 
+
 @pytest.fixture(scope='session')
 def configure_with_daemon(configure):
     with handle_daemon():
         yield
+
 
 @pytest.fixture(scope='session')
 def configure():
@@ -40,8 +42,7 @@ def configure():
                     db_port=pgt.port,
                     db_name='postgres',
                     db_pass='',
-                    repo=str(td)
-                )
+                    repo=str(td))
 
             from ._computer import setup_computer
             computers = config.get('computers', [])
@@ -54,20 +55,28 @@ def configure():
                 setup_code(**code_kwargs)
             yield
 
+
 @contextmanager
 def reset_config_after_run():
     config_folder = os.path.expanduser(aiida.common.setup.AIIDA_CONFIG_FOLDER)
-    config_save_folder = os.path.join(os.path.dirname(config_folder), '.aiida~')
+    config_save_folder = os.path.join(
+        os.path.dirname(config_folder), '.aiida~')
     reset_config(config_folder, config_save_folder)
     assert not os.path.isfile(os.path.join(config_folder, 'config.json'))
     shutil.copytree(config_folder, config_save_folder)
     yield
     reset_config(config_folder, config_save_folder)
 
+
 def reset_config(config_folder, config_save_folder):
     if os.path.isdir(config_save_folder):
+        # remove temp `submit_test` folder for not_submitted_to_daemon tests
+        shutil.rmtree(os.path.join(os.path.dirname(config_folder), 'submit_test'))
+
+        # remove temp `.aiida` folder and reset configuration
         shutil.rmtree(config_folder, ignore_errors=True)
         os.rename(config_save_folder, config_folder)
+
 
 @contextmanager
 def handle_daemon():
