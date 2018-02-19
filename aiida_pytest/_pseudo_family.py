d643477c3677c88dd0fc1f921cd7354bfd27d60b
Dominik Gresch <greschd@gmx.ch>
2018-02-19 12:03:17 +0100

    Use CLASS_LOADER instead of plumpy load_class

--- aiida_pytest/_pseudo_family.py
+++ aiida_pytest/_pseudo_family.py
@@ -3,11 +3,11 @@
 
 import os
 
-from plum.util import load_class
+from aiida.work.class_loader import CLASS_LOADER
 
 from .contextmanagers import redirect_stdout
 
 def setup_pseudo_family(command_name, folder, group_name, group_description):
-    pseudo_cmd = load_class(command_name)()
+    pseudo_cmd = CLASS_LOADER.load_class(command_name)()
     with open(os.devnull, 'w') as devnull, redirect_stdout(devnull):
         pseudo_cmd.uploadfamily(folder, group_name, group_description)
