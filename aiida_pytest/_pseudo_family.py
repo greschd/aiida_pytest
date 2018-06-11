0f932b3ee9ef2be653af83ef1334b1e692cbee6c
Dominik Gresch <greschd@gmx.ch>
2018-06-11 16:31:10 +0200

    Use 'get_object_from_string' to replace CLASS_LOADER instead.

--- aiida_pytest/_pseudo_family.py
+++ aiida_pytest/_pseudo_family.py
@@ -3,11 +3,11 @@
 
 import os
 
-from aiida.work import ObjectLoader
+from aiida.common.utils import get_object_from_string
 
 from .contextmanagers import redirect_stdout
 
 def setup_pseudo_family(command_name, folder, group_name, group_description):
-    pseudo_cmd = ObjectLoader().load_object(command_name)()
+    pseudo_cmd = get_object_from_string(command_name)()
     with open(os.devnull, 'w') as devnull, redirect_stdout(devnull):
         pseudo_cmd.uploadfamily(folder, group_name, group_description)
