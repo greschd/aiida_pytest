823d8066fd37288132e31314de511c2cf991538d
Dominik Gresch <greschd@gmx.ch>
2020-10-28 13:52:36 +0100

    Bump version number to 0.1.0a8.

--- aiida_pytest/__init__.py
+++ aiida_pytest/__init__.py
@@ -4,7 +4,7 @@
 # © 2017-2019, ETH Zurich, Institut für Theoretische Physik
 # Author: Dominik Gresch <greschd@gmx.ch>
 
-__version__ = '0.1.0a7'
+__version__ = '0.1.0a8'
 
 from ._process import *
 from ._configure import *
